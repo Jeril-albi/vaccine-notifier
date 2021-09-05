6c71645c7655ba7b4f55b8c6bb5aebd22cd94a71
Jeril Albi <devjerilalbi@gmail.com>
2021-09-05 16:03:54 +0530

    prbs solved

--- lib/home.dart
+++ lib/home.dart
@@ -9,98 +9,123 @@ import 'package:flutter/services.dart';
 
 class Home extends StatefulWidget {
   @override
-  _home createState() => _home();
+  _Home createState() => _Home();
 }
 
-class _home extends State<Home> with WidgetsBindingObserver{
+class _Home extends State<Home> with WidgetsBindingObserver {
+  bool validate = false;
+  Timer timer;
+  Widgets wid = Widgets();
 
- bool validate = false;
- Timer timer;
- Widgets wid = Widgets();
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+        builder: (BuildContext context, Widget child) {
+          return Theme(
+              child: child,
+              data: ThemeData.dark().copyWith(
+                colorScheme: ColorScheme.dark(
+                  primary: Colors.blueAccent,
+                  onPrimary: Colors.white,
+                  surface: Colors.blue,
+                ),
+              ));
+        },
+        context: context,
+        initialDate: Widgets.searchSelectedDate,
+        firstDate: DateTime(2015, 8),
+        lastDate: DateTime(2101));
 
+    if (picked != null && picked != Widgets.searchSelectedDate)
+      setState(() {
+        Widgets.searchSelectedDate = picked;
+      });
+  }
 
- Future<void> _selectDate(BuildContext context) async {
-   final DateTime picked = await showDatePicker(
-       builder: (BuildContext context, Widget child) {
-         return Theme(
-             child: child,
-             data: ThemeData.dark().copyWith(
-               colorScheme: ColorScheme.dark(
-                 primary: Colors.blueAccent,
-                 onPrimary: Colors.white,
-                 surface: Colors.blue,
-               ),
-             )
-         );
-       },
-       context: context,
-       initialDate: Widgets.searchSelectedDate,
-       firstDate: DateTime(2015, 8),
-       lastDate: DateTime(2101));
+  fetchSlots() async {
+    var response = await Dio().get(
+        "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=${Widgets.searchPinCodeTaker.text}&date=${Widgets.searchSelectedDate.day}-${Widgets.searchSelectedDate.month}-${Widgets.searchSelectedDate.year}");
+    setState(() {
+      Widgets.searchData = response.data['sessions'];
+    });
+    Navigator.push(context, MaterialPageRoute(builder: (context) => Slots()));
+  }
 
-   if (picked != null && picked != Widgets.searchSelectedDate)
-     setState(() {
-       Widgets.searchSelectedDate = picked;
-     });
- }
-
- fetchSlots() async{
-   var response = await Dio().get("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=${Widgets.searchPinCodeTaker.text}&date=${Widgets.searchSelectedDate.day}-${Widgets.searchSelectedDate.month}-${Widgets.searchSelectedDate.year}");
-   setState(() {
-     Widgets.searchData = response.data['sessions'];
-   });
-   Navigator.push(context, MaterialPageRoute(builder: (context) => Slots()));
- }
-
- checkSlots( String pincode) async{
-   if(Widgets.notify == true){
-     var response = await Dio().get("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=$pincode&date=${Widgets.date.day}-${Widgets.date.month}-${Widgets.date.year}");
+  checkSlots(String pincode) async {
+    if (Widgets.notify == true) {
+      var response = await Dio().get(
+          "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=$pincode&date=${Widgets.date.day}-${Widgets.date.month}-${Widgets.date.year}");
       setState(() {
         Widgets.validate1 = false;
         Widgets.firstData = response.data['sessions'];
       });
-     for(int i = 0; i<  Widgets.firstData.length; i++){
-       if(Widgets.age == null && Widgets.vaccineName == null && Widgets.dose == null && Widgets.firstData[i]['available_capacity'] != 0 && Widgets.firstData[i]['fee'] == '0'){
-         wid.showNotification( Widgets.firstData[i]['name'].toString(), i ,Widgets.firstData[i]['available_capacity'].toString(),Widgets.firstData[i]['available_capacity_dose1'].toString(),Widgets.firstData[i]['available_capacity_dose2'].toString(),Widgets.firstData[i]['vaccine'].toString(),Widgets.firstData[i]['min_age_limit'].toString());
-       }
-       if(Widgets.firstData[i]['available_capacity'] != 0 && Widgets.firstData[i]['available_capacity_${Widgets.dose}'] != 0 && Widgets.firstData[i]['vaccine'] == Widgets.vaccineName && Widgets.firstData[i]['min_age_limit'] == Widgets.age && Widgets.firstData[i]['fee'] == '0'){
-         wid.showNotification( Widgets.firstData[i]['name'].toString(), i ,Widgets.firstData[i]['available_capacity'].toString(),Widgets.firstData[i]['available_capacity_dose1'].toString(),Widgets.firstData[i]['available_capacity_dose2'].toString(),Widgets.firstData[i]['vaccine'].toString(),Widgets.firstData[i]['min_age_limit'].toString());
-       }
-     }
-   } else {
-     return null;
-   }
- }
+      for (int i = 0; i < Widgets.firstData.length; i++) {
+        if (Widgets.age == null &&
+            Widgets.vaccineName == null &&
+            Widgets.dose == null &&
+            Widgets.firstData[i]['available_capacity'] != 0 &&
+            Widgets.firstData[i]['fee'] == '0') {
+          wid.showNotification(
+              Widgets.firstData[i]['name'].toString(),
+              i,
+              Widgets.firstData[i]['available_capacity'].toString(),
+              Widgets.firstData[i]['available_capacity_dose1'].toString(),
+              Widgets.firstData[i]['available_capacity_dose2'].toString(),
+              Widgets.firstData[i]['vaccine'].toString(),
+              Widgets.firstData[i]['min_age_limit'].toString());
+        }
+        if (Widgets.firstData[i]['available_capacity'] != 0 &&
+            Widgets.firstData[i]['available_capacity_${Widgets.dose}'] != 0 &&
+            Widgets.firstData[i]['vaccine'] == Widgets.vaccineName &&
+            Widgets.firstData[i]['min_age_limit'] == Widgets.age &&
+            Widgets.firstData[i]['fee'] == '0') {
+          wid.showNotification(
+              Widgets.firstData[i]['name'].toString(),
+              i,
+              Widgets.firstData[i]['available_capacity'].toString(),
+              Widgets.firstData[i]['available_capacity_dose1'].toString(),
+              Widgets.firstData[i]['available_capacity_dose2'].toString(),
+              Widgets.firstData[i]['vaccine'].toString(),
+              Widgets.firstData[i]['min_age_limit'].toString());
+        }
+      }
+    } else {
+      return null;
+    }
+  }
 
- @override
- void initState() {
-   super.initState();
-   WidgetsBinding.instance.addObserver(this);
- }
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
 
- @override
- void dispose() {
-   WidgetsBinding.instance.removeObserver(this);
-   super.dispose();
- }
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
 
- @override
- void didChangeAppLifecycleState(AppLifecycleState state) {
-   super.didChangeAppLifecycleState(state);
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
 
-   if(state == AppLifecycleState.inactive || state == AppLifecycleState.detached)return;
-   final isBackground = state == AppLifecycleState.paused;
+    if (state == AppLifecycleState.inactive ||
+        state == AppLifecycleState.detached) return;
+    final isBackground = state == AppLifecycleState.paused;
 
-   if(isBackground){
-     if( Widgets.pinCodeTaker1 != null){
-       Widgets.timer1 = Timer.periodic(Duration(seconds: 30), (Timer t ) => checkSlots( Widgets.pinCodeTaker1.text));
-     } else return null;
-     if( Widgets.pinCodeTaker2 != null){
-       Widgets.timer2 = Timer.periodic(Duration(seconds: 60), (Timer t ) => checkSlots( Widgets.pinCodeTaker2.text));
-     } else return null;
-   }
-
- }
+    if (isBackground) {
+      if (Widgets.pinCodeTaker1 != null) {
+        Widgets.timer1 = Timer.periodic(Duration(seconds: 30),
+            (Timer t) => checkSlots(Widgets.pinCodeTaker1.text));
+      } else
+        return null;
+      if (Widgets.pinCodeTaker2 != null) {
+        Widgets.timer2 = Timer.periodic(Duration(seconds: 60),
+            (Timer t) => checkSlots(Widgets.pinCodeTaker2.text));
+      } else
+        return null;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -110,100 +135,134 @@ class _home extends State<Home> with WidgetsBindingObserver{
       DeviceOrientation.portraitDown,
     ]);
     return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        title: Text("CoWin Notify"),
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.4), BlendMode.dstATop),
-              image: AssetImage('assets/background.png'),
-              fit: BoxFit.cover
-            )
-          ),
-          padding: EdgeInsets.all(15),
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: 20,),
-              Container(
-                height: 50,
-                child: TextField(
-                  style: TextStyle(color: Colors.white),
-                  keyboardType: TextInputType.number,
-                  controller: Widgets.searchPinCodeTaker,
-                  decoration: InputDecoration(
-                    fillColor: Colors.white,
-                    hintStyle: TextStyle(color: Colors.white),
-                    hintText: "Enter Pincode",
-                    errorText: validate ? 'Enter correct pincode' : null,
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: Text("CoWin Notify"),
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    colorFilter: ColorFilter.mode(
+                        Colors.black.withOpacity(0.4), BlendMode.dstATop),
+                    image: AssetImage('assets/background.png'),
+                    fit: BoxFit.cover)),
+            padding: EdgeInsets.all(15),
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  height: 50,
+                  child: TextField(
+                    style: TextStyle(color: Colors.white),
+                    keyboardType: TextInputType.number,
+                    controller: Widgets.searchPinCodeTaker,
+                    decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      hintStyle: TextStyle(color: Colors.white),
+                      hintText: "Enter Pincode",
+                      errorText: validate ? 'Enter correct pincode' : null,
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(height: 10,),
-              Text("Select Date",style: TextStyle(color: Colors.white,fontSize: 18),),
-              SizedBox(height: 10,),
-              Row(
-                children: [
-                  IconButton(icon: Icon(Icons.date_range,color: Colors.white,),
-                      onPressed: (){ _selectDate(context);}
-                  ),
-                  SizedBox(width: 30,),
-                  Text("Date: ${Widgets.searchSelectedDate.day} - ${Widgets.searchSelectedDate.month} - ${Widgets.searchSelectedDate.year}",style: TextStyle(color: Colors.white,fontSize: 18),),
-                ],
-              ),
-              SizedBox(height: 20,),
-              Container(
-                height: 45,
-                width: double.infinity,
-                child: ElevatedButton(
-                    onPressed: (){
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "Select Date",
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: [
+                    IconButton(
+                        icon: Icon(
+                          Icons.date_range,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {
+                          _selectDate(context);
+                        }),
+                    SizedBox(
+                      width: 30,
+                    ),
+                    Text(
+                      "Date: ${Widgets.searchSelectedDate.day} - ${Widgets.searchSelectedDate.month} - ${Widgets.searchSelectedDate.year}",
+                      style: TextStyle(color: Colors.white, fontSize: 18),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  height: 45,
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {
                       setState(() {
-                        if(Widgets.searchPinCodeTaker.text.isEmpty || Widgets.searchPinCodeTaker.text.length != 6){
+                        if (Widgets.searchPinCodeTaker.text.isEmpty ||
+                            Widgets.searchPinCodeTaker.text.length != 6) {
                           validate = true;
-                        }else {
+                        } else {
                           fetchSlots();
                           validate = false;
                         }
                       });
-                      },
+                    },
                     child: Text("Search"),
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.blue)
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.blue)),
                   ),
                 ),
-              ),
-              SizedBox(height: 40,),
-              InkWell(
-                onTap: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => NotificationScreen()));
+                SizedBox(
+                  height: 40,
+                ),
+                InkWell(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => NotificationScreen()));
                   },
-                child: Container(
-                  height: 35,
-                  width: 155,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(50),
-                        border: Border.all(color: Colors.white)
+                  child: Container(
+                    height: 35,
+                    width: 155,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        border: Border.all(color: Colors.white)),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Set Notification",
+                          style: TextStyle(color: Colors.white, fontSize: 15),
+                        ),
+                        Divider(),
+                        Icon(
+                          Widgets.notify
+                              ? Icons.notifications_active_sharp
+                              : Icons.notifications_active_outlined,
+                          color: Colors.white,
+                        )
+                      ],
+                    ),
                   ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                    Text("Set Notification",style: TextStyle(color: Colors.white,fontSize: 15),),
-                    Divider(),
-                    Icon( Widgets.notify ?
-                    Icons.notifications_active_sharp: Icons.notifications_active_outlined,color: Colors.white,)
-                  ],),
                 ),
-              ),
-              SizedBox(height: 20,),
-            ],
+                SizedBox(
+                  height: 20,
+                ),
+              ],
+            ),
           ),
-        ),
-      )
-    );
+        ));
   }
 }
--- lib/slots.dart
+++ lib/slots.dart
@@ -1,16 +1,13 @@
-import 'dart:convert';
-
 import 'package:auto_vaccine/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class Slots extends StatefulWidget{
-  @override 
+class Slots extends StatefulWidget {
+  @override
   _Slots createState() => _Slots();
 }
 
-class _Slots extends State<Slots>{
-
+class _Slots extends State<Slots> {
   Widgets wid = Widgets();
 
   @override
@@ -30,45 +27,90 @@ class _Slots extends State<Slots>{
         padding: EdgeInsets.all(15),
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
-        child: Widgets.searchData.length != 0 ? ListView.builder(
-          itemCount: Widgets.searchData.length,
-            itemBuilder: (context,index){
-              return InkWell(
-                onTap: (){
-                  wid.showNotification(Widgets.searchData[index]['name'].toString(), index ,Widgets.searchData[index]['available_capacity'].toString(),Widgets.searchData[index]['available_capacity_dose1'].toString(),Widgets.searchData[index]['available_capacity_dose2'].toString(),Widgets.searchData[index]['vaccine'].toString(),Widgets.searchData[index]['min_age_limit'].toString());
-                  },
-                child: Container(
-                margin: EdgeInsets.all(5),
-                height: 230,
-                color: Widgets.searchData[index]['available_capacity'] != 0 ? Colors.red[900] : Colors.grey[850],
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("Center  Name: ${Widgets.searchData[index]['name'].toString()}",style: TextStyle(fontSize: 15,color: Colors.white),),
-                    Divider(),
-                    Text("Available Slots: ${Widgets.searchData[index]['available_capacity'].toString()} ( D1: ${Widgets.searchData[index]['available_capacity_dose1'].toString()}, D2: ${Widgets.searchData[index]['available_capacity_dose2'].toString()} )",style: TextStyle(fontSize: 15,color: Colors.white),),
-                    Divider(),
-                    Text("Status: ${Widgets.searchData[index]['fee_type'].toString()} ( ₹ ${Widgets.searchData[index]['fee'].toString()} )",style: TextStyle(fontSize: 15,color: Colors.white),),
-                    Divider(),
-                    Text("Vaccine Name: ${Widgets.searchData[index]['vaccine'].toString()}",style: TextStyle(fontSize: 15,color: Colors.white),),
-                    Divider(),
-                    Text("Min Age: ${Widgets.searchData[index]['min_age_limit'].toString()} ",style: TextStyle(fontSize: 15,color: Colors.white),),
-                    Divider(),
-                    Text("Time : ${Widgets.searchData[index]['slots'].toString()}",style: TextStyle(fontSize: 15,color: Colors.white),),
-                  ],
-                ),
-            ),
-              );
-            }
-        ) : Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("No centers available on",style: TextStyle(color: Colors.white,fontSize: 25),),
-            Text("${Widgets.searchPinCodeTaker.text}",style: TextStyle(color: Colors.white,fontSize: 25),),
-            Text("in",style: TextStyle(color: Colors.white,fontSize: 25),),
-            Text("${Widgets.searchSelectedDate.day}-${Widgets.searchSelectedDate.month}-${Widgets.searchSelectedDate.year}",style: TextStyle(color: Colors.white,fontSize: 25),),
-          ],
-        ),
+        child: Widgets.searchData.length != 0
+            ? ListView.builder(
+                itemCount: Widgets.searchData.length,
+                itemBuilder: (context, index) {
+                  return InkWell(
+                    onTap: () {
+                      wid.showNotification(
+                          Widgets.searchData[index]['name'].toString(),
+                          index,
+                          Widgets.searchData[index]['available_capacity']
+                              .toString(),
+                          Widgets.searchData[index]['available_capacity_dose1']
+                              .toString(),
+                          Widgets.searchData[index]['available_capacity_dose2']
+                              .toString(),
+                          Widgets.searchData[index]['vaccine'].toString(),
+                          Widgets.searchData[index]['min_age_limit']
+                              .toString());
+                    },
+                    child: Container(
+                      margin: EdgeInsets.all(5),
+                      height: 230,
+                      color:
+                          Widgets.searchData[index]['available_capacity'] != 0
+                              ? Colors.red[900]
+                              : Colors.grey[850],
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Center  Name: ${Widgets.searchData[index]['name'].toString()}",
+                            style: TextStyle(fontSize: 15, color: Colors.white),
+                          ),
+                          Divider(),
+                          Text(
+                            "Available Slots: ${Widgets.searchData[index]['available_capacity'].toString()} ( D1: ${Widgets.searchData[index]['available_capacity_dose1'].toString()}, D2: ${Widgets.searchData[index]['available_capacity_dose2'].toString()} )",
+                            style: TextStyle(fontSize: 15, color: Colors.white),
+                          ),
+                          Divider(),
+                          Text(
+                            "Status: ${Widgets.searchData[index]['fee_type'].toString()} ( ₹ ${Widgets.searchData[index]['fee'].toString()} )",
+                            style: TextStyle(fontSize: 15, color: Colors.white),
+                          ),
+                          Divider(),
+                          Text(
+                            "Vaccine Name: ${Widgets.searchData[index]['vaccine'].toString()}",
+                            style: TextStyle(fontSize: 15, color: Colors.white),
+                          ),
+                          Divider(),
+                          Text(
+                            "Min Age: ${Widgets.searchData[index]['min_age_limit'].toString()} ",
+                            style: TextStyle(fontSize: 15, color: Colors.white),
+                          ),
+                          Divider(),
+                          Text(
+                            "Time : ${Widgets.searchData[index]['slots'].toString()}",
+                            style: TextStyle(fontSize: 15, color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                })
+            : Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "No centers available on",
+                    style: TextStyle(color: Colors.white, fontSize: 25),
+                  ),
+                  Text(
+                    "${Widgets.searchPinCodeTaker.text}",
+                    style: TextStyle(color: Colors.white, fontSize: 25),
+                  ),
+                  Text(
+                    "in",
+                    style: TextStyle(color: Colors.white, fontSize: 25),
+                  ),
+                  Text(
+                    "${Widgets.searchSelectedDate.day}-${Widgets.searchSelectedDate.month}-${Widgets.searchSelectedDate.year}",
+                    style: TextStyle(color: Colors.white, fontSize: 25),
+                  ),
+                ],
+              ),
       ),
     );
   }
